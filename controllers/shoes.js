8e343e2e09ddeed7ac815e526397592948d3d310
msushmanorthwest2023 <msushma2023@gmail.com>
2023-11-15 01:17:28 -0600

    Update shoes.js

--- controllers/shoes.js
+++ controllers/shoes.js
@@ -22,6 +22,19 @@ exports.shoes_delete = async function(req, res) {
     res.send(`{"error": Error deleting ${err}}`);
     }
     };
+    // Handle a show one view with id specified by query
+exports.shoes_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await shoes.findById( req.query.id)
+    res.render('shoesdetail',
+    { title: 'shoes Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
     
 // for a specific shoes.
 exports.shoes_detail = async function(req, res) {
