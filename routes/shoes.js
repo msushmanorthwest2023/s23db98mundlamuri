26fb6665c6d32a16acc19e9f63f8a1abf3516305
msushmanorthwest2023 <msushma2023@gmail.com>
2023-11-15 19:18:48 -0600

    Update shoes.js

--- routes/shoes.js
+++ routes/shoes.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const shoes_controlers= require('../controllers/shoes');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('shoes', { title: 'Search Results shoes' });
 });
-var express = require('express');
-const shoes_controlers= require('../controllers/shoes');
-var router = express.Router();
+
 /* GET shoess */
 router.get('/', shoes_controlers.shoes_view_all_Page );
 module.exports = router;
@@ -16,3 +16,6 @@ router.get('/detail', shoes_controlers.shoes_view_one_Page);
 
 /* GET create shoes page */
 router.get('/create', shoes_controlers.shoes_create_Page);
+
+/* GET create update page */
+router.get('/update', shoes_controlers.shoes_update_Page);
