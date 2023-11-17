b292b22da711f2ec2e96a53505960eba70662a3f
msushmanorthwest2023 <msushma2023@gmail.com>
2023-11-17 12:33:46 -0600

    changed

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ passport.use(new LocalStrategy(
 // passport config
 // Use the existing connection
 // The Account model
-var Account =require('./models/account'));
+var Account =require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
