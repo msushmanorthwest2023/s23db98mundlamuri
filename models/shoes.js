20ab57d969e5bffffc643a65b524fbe9e85f4f29
msushmanorthwest2023 <msushma2023@gmail.com>
2023-11-29 17:21:57 -0600

    Update shoes.js

--- models/shoes.js
+++ models/shoes.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose")
 const shoesSchema = mongoose.Schema({
 shoe_brand: String,
 shoe_size: String,
-shoe_price: Number
-})
-module.exports = mongoose.model("shoes",shoesSchema)
+shoe_price: {
+    type: Number,
+    required: true,
+    min: [0, 'Price must be non-negative'],//Minimum value of 0
+    max: [1000, 'Price cannot exceed 1000'],//Maximum value of 1000
+},
+});
+
+module.exports = mongoose.model("shoes",shoesSchema);
